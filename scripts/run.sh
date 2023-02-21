27ef597f9993687dc3bab9895948f1d00c20350d
Abirdcfly <fp544037857@gmail.com>
2023-02-21 18:04:44 +0800

    偷懒1

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-docker pull gcr.io/tekton-releases/github.com/tektoncd/operator/cmd/kubernetes/operator:v0.64.0
-docker tag gcr.io/tekton-releases/github.com/tektoncd/operator/cmd/kubernetes/operator:v0.64.0 hyperledgerk8s/tekton-releases/github.com/tektoncd/operator/cmd/kubernetes/operator:v0.64.0
-docker push hyperledgerk8s/tekton-releases/github.com/tektoncd/operator/cmd/kubernetes/operator:v0.64.0 || true
+docker pull gcr.io/tekton-releases/github.com/tektoncd/operator/cmd/kubernetes/webhook:v0.64.0
+docker tag gcr.io/tekton-releases/github.com/tektoncd/operator/cmd/kubernetes/webhook:v0.64.0 hyperledgerk8s/tekton-operator-webhook:v0.64.0
+docker push hyperledgerk8s/tekton-operator-webhook:v0.64.0 || true

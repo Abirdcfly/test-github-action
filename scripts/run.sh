205fa2396279585a910b7aab1f25bf41d01e6005
Abirdcfly <fp544037857@gmail.com>
2023-02-21 17:50:36 +0800

    偷懒

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-export UPLOAD_IMAGE=NO
-echo "UPLOAD_IMAGE=YES" >> $GITHUB_ENV
+docker pull gcr.io/tekton-releases/github.com/tektoncd/operator/cmd/kubernetes/operator:v0.64.0
+docker tag gcr.io/tekton-releases/github.com/tektoncd/operator/cmd/kubernetes/operator:v0.64.0 hyperledgerk8s/tekton-releases/github.com/tektoncd/operator/cmd/kubernetes/operator:v0.64.0
+docker push hyperledgerk8s/tekton-releases/github.com/tektoncd/operator/cmd/kubernetes/operator:v0.64.0 || true

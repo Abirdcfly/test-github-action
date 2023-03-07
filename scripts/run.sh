15f6b4d8b79fb20aa3084e0b64e84d31b3ed1ee6
Abirdcfly <fp544037857@gmail.com>
2023-03-07 09:25:29 +0800

    debug

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 IMAGES=(
-gcr.io/kaniko-project/executor:v1.5.1@sha256:c6166717f7fe0b7da44908c986137ecfeab21f31ec3992f6e128fff8a94be8a5
-docker.io/library/bash:5.1.4@sha256:c523c636b722339f41b6a431b44588ab2f762c5de5ec3bd7964420ff982fb1d9
+ubuntu:22.04
 )
 
 NEW_IMAGES=(
-hyperledgerk8s/kaniko-executor:v1.5.1
-hyperledgerk8s/bash:5.1.4
+hyperledgerk8s/ubuntu:22.04
 )
 
 for ((i=0;i<${#IMAGES[@]};++i)); do

32016a07e9a12a8d4275fa2e6f83a5fd7a67d296
Abirdcfly <fp544037857@gmail.com>
2023-03-09 14:39:59 +0800

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 IMAGES=(
-gcr.io/kaniko-project/executor:v1.9.1
+mcr.microsoft.com/powershell:nanoserver@sha256:b6d5ff841b78bdf2dfed7550000fd4f3437385b8fa686ec0f010be24777654d6
 )
 
 NEW_IMAGES=(
-hyperledgerk8s/kaniko-executor:v1.9.1
+hyperledgerk8s/tekton-pipeline-args-powershell:nanoserver-b6d5ff
 )
 
 for ((i=0;i<${#IMAGES[@]};++i)); do

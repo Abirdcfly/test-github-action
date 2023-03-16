5869262b0ada6d3d7ca650017916c9e8ffc7c3d9
Abirdcfly <fp544037857@gmail.com>
2023-03-16 09:25:46 +0800

    dd

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
-IMAGES=(
-mcr.microsoft.com/powershell:nanoserver-1809@sha256:a3c7e83f8018352a6284cefdf5066602bff9b298edf3cd51085c74a19cfafffc
-)
+#IMAGES=(
+#mcr.microsoft.com/powershell:nanoserver-1809@sha256:a3c7e83f8018352a6284cefdf5066602bff9b298edf3cd51085c74a19cfafffc
+#)
 
-NEW_IMAGES=(
-hyperledgerk8s/tekton-pipeline-args-powershell:nanoserver-1809
-)
+#NEW_IMAGES=(
+#hyperledgerk8s/tekton-pipeline-args-powershell:nanoserver-1809
+#)
 
-for ((i=0;i<${#IMAGES[@]};++i)); do
-  docker pull ${IMAGES[$i]}
-  docker tag ${IMAGES[$i]} ${NEW_IMAGES[$i]}
-  docker push ${NEW_IMAGES[$i]} || true
-done
+#for ((i=0;i<${#IMAGES[@]};++i)); do
+#  docker pull ${IMAGES[$i]}
+#  docker tag ${IMAGES[$i]} ${NEW_IMAGES[$i]}
+#  docker push ${NEW_IMAGES[$i]} || true
+#done
 
+kind create cluster --image kindest/node:v1.24.4
+docker inspect kind-control-plane

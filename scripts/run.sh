3806e3df511636653455beb3000f0fd32413eae5
Abirdcfly <fp544037857@gmail.com>
2023-03-16 13:24:14 +0800

    test3

--- scripts/run.sh
+++ scripts/run.sh
@@ -38,7 +38,7 @@ function save_all_images() {
 		for image in ${IMAGES[@]}; do
 			n=$((n + 1))
 			for dockerContainerName in ${dockerContainerNames[@]}; do
-				docker exec -i ${dockerContainerName} ctr --namespace=k8s.io images export --skip-manifest-json --skip-non-distributable ${outputDir}/${n}.tar ${image}) || exit_status=$?
+				docker exec -i ${dockerContainerName} ctr --namespace=k8s.io images export --skip-manifest-json --skip-non-distributable ${outputDir}/${n}.tar ${image} || exit_status=$?
 				echo $image $exit_status $dockerContainerName
 				if [[ $exit_status -eq 0 ]]; then
 					break
